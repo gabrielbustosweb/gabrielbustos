ec5447ea22df5719e4f6e4f061be6d8407cf8273
Gabriel Bustos <gabrielbustos01@gmail.com>
2024-09-27 21:36:47 -0300

    add header

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,6 +1,10 @@
 import { Outlet } from 'react-router-dom'
 import CoverParticles from '../components/CoverParticles'
 import Header from '../components/Header'
+import { MotionTransition } from '../components/TransitionComponent'
+import { NavLink } from 'react-router-dom'
+import { HomeIcon, BriefcaseIcon, EnvelopeIcon } from '@heroicons/react/24/outline'
+
 
 export default function Layout() {
     return (
@@ -8,6 +12,28 @@ export default function Layout() {
             <CoverParticles />
             <div className="relative z-10">
                 <Header />
+                <MotionTransition position="right" className="fixed z-40 flex flex-col items-center justify-center w-full mt-auto bottom-10">
+                    <nav className="flex items-center justify-center gap-2 px-4 py-1 rounded-full bg-white/15 backdrop-blur-sm">
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) => isActive ? 'bg-secondary rounded-full' : ''}
+                        >
+                            <HomeIcon className="w-9 h-9 py-1 px-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary" />
+                        </NavLink>
+                        <NavLink
+                            to="/projects"
+                            className={({ isActive }) => isActive ? 'bg-secondary rounded-full' : ''}
+                        >
+                            <BriefcaseIcon className="w-9 h-9 py-1 px-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary" />
+                        </NavLink>
+                        <NavLink
+                            to="/contact"
+                            className={({ isActive }) => isActive ? 'bg-secondary rounded-full' : ''}
+                        >
+                            <EnvelopeIcon className="w-9 h-9 py-1 px-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary" />
+                        </NavLink>
+                    </nav>
+                </MotionTransition>
                 <main>
                     <Outlet />
                 </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,15 @@ export default {
       colors: {
         'secondary': '#f3e500',
         'gbDark': '#2b2a5c',
-        'gbLight': '#dad0c0'
+        'gbLight': '#dad0c0',
+        'social': '#145e98'
       },
       backgroundImage: {
         'gradient-cover':
           'linear-gradient(to right, #2b2a5c, #dad0c0)',
       },
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: ['Urbanist', 'sans-serif'],
       },
       animation: {
         'marquee': 'marquee 25s linear infinite',
